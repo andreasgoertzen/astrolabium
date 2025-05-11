85ad4e35180d7aafbf7a079073b30633e64c56c1
Andreas Goertzen <mail@AndreasGoertzen.eu>
2025-05-11 15:16:50 +0200

    add exampleStarMap

--- src/main.js
+++ src/main.js
@@ -2,20 +2,19 @@ import './style.css'
 import javascriptLogo from '/src/assets/svg/javascript.svg'
 import { setupCounter } from '/src/util/counter.js'
 import viteLogo from '/svg/vite.svg'
-import exampleBars from "/src/viz/exampleBars.js";
+import exampleStarMap from "@/viz/exampleStarMap.js";
 
 document.querySelector('#app').innerHTML = `
   <div>
       <div class="flex flex-row gap-2">
         <img src="${viteLogo}" class="logo" alt="Vite logo" />
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-        <h class="text-xl">Moin Vite!</h1>
+        <h1 class="text-3xl font-bold">Astrolabium</h1>
         <button class="btn btn-primary" id="counter" type="button"></button>
       </div>
       <div id="viz-container"></div>
   </div>
 `
-
 setupCounter(document.querySelector('#counter'));
 
-exampleBars();
+exampleStarMap();
