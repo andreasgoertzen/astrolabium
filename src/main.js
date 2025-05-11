344ae227b772f707aa8fcc06fc54c2ccdce53b94
Andreas Goertzen <mail@AndreasGoertzen.eu>
2025-05-11 11:30:32 +0200

    add tailwind, daisyui

--- src/main.js
+++ src/main.js
@@ -1,23 +1,16 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import javascriptLogo from '/src/assets/svg/javascript.svg'
+import viteLogo from '/svg/vite.svg'
+import { setupCounter } from '/src/util/counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+      <div class="flex flex-row gap-2">
+        <img src="${viteLogo}" class="logo" alt="Vite logo" />
+        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+        <h class="text-xl">Moin Vite!</h1>
+        <button class="btn btn-outline" id="counter" type="button"></button>
+      </div>
   </div>
 `
 
