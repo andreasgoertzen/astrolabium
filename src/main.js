541b7c85c87fba9f9531bd4d9aa5db05a7227dda
Andreas Goertzen <mail@AndreasGoertzen.eu>
2025-05-11 13:39:04 +0200

    add d3

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from '/src/assets/svg/javascript.svg'
 import viteLogo from '/svg/vite.svg'
 import { setupCounter } from '/src/util/counter.js'
+import { showBarChart } from "/src/viz/bar.js";
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -9,9 +10,12 @@ document.querySelector('#app').innerHTML = `
         <img src="${viteLogo}" class="logo" alt="Vite logo" />
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
         <h class="text-xl">Moin Vite!</h1>
-        <button class="btn btn-outline" id="counter" type="button"></button>
+        <button class="btn btn-primary" id="counter" type="button"></button>
       </div>
+      <div class="border p-3 mt-2" id="viz-container"></div>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'));
+
+showBarChart();
