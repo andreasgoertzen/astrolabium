56643eda5ba6c7f236e89d59e75d4bab97cd74f3
Andreas Goertzen <mail@AndreasGoertzen.eu>
2025-05-17 04:11:09 +0200

    add messier

--- src/astro/astro.js
+++ src/astro/astro.js
@@ -1,22 +1,33 @@
+import { BACKGROUND_COLOR, PROJECTION_COLOR } from "@/astro/const.js";
 import { bvToHex, isPointVisible } from "@/util/calc.js";
 import { logj } from "@/util/log.js";
 import { renderTextGraticule } from "@/util/render.js";
 import Zoomer from "@/util/Zoomer.js";
+import simplify from "@turf/simplify";
 import * as d3 from 'd3';
 
 export default async () => {
-  const config = {}
+  let showMilkyWay = true;
+
   const data = {
+    asterisms: (await d3.json("/data/asterisms.json")).features,
     constellations: (await d3.json("/data/constellations.json")).features,
     constellationLines: (await d3.json("/data/constellations.lines.json")).features,
+    constellationBounds: (await d3.json("/data/constellations.borders.json")).features,
+    dsonames: (await d3.json("/data/dsonames.json")),
     graticuleLines: d3.geoGraticule().step([15, 10]).lines(),
+    messier: (await d3.json("/data/messier.json")).features,
+    milkyWay: (await d3.json("/data/milkyway.json")).features,
+    milkyWaySimple: [],
     stars: (await d3.json("/data/stars.6.json")).features,
     starNames: (await d3.json("/data/starnames.json")),
     selectedStars: []
   };
   const scale = {
-    magnitude: d3.scaleLinear(d3.extent(data.stars, d => d.properties.mag), [4, 0.5])
+    magnitude: d3.scaleLinear(d3.extent(data.stars, d =>d.properties.mag), [4, 0.5])
   }
+
+  // logj(data.milkyWay)
 
   let width = window.innerWidth;
   let height = window.innerHeight;
@@ -29,6 +40,8 @@ export default async () => {
     .clipAngle(90)
     .reflectX(true)
   ;
+  let unityScale = projection.scale();
+  function zoomFactor() { return projection.scale() / unityScale;}
   const geoPath = d3
     .geoPath()
     .projection(projection)
@@ -41,21 +54,140 @@ export default async () => {
     x.properties.r = scale.magnitude(x.properties.mag);
   })
 
+  data.milkyWay.forEach(x => simplify(x, {
+    tolerance: 10,
+    highQuality: false
+  }))
+
   const render = () => {
     ctx.clearRect(0, 0, width, height);
-    // black background
+
+    // background
     ctx.beginPath();
-    ctx.fillStyle = '#000';
-    ctx.beginPath();
+    ctx.fillStyle = BACKGROUND_COLOR;
     ctx.fillRect(0, 0, width, height);
-    // graticule
-    ctx.strokeStyle = d3.color('gray').darker(0.5);
+
+    // projection background
     ctx.beginPath();
+    ctx.fillStyle = PROJECTION_COLOR;
+    ctx.arc(width / 2, height / 2, projection.scale(), 0, 2 * Math.PI);
+    ctx.fill();
+
+    // graticule
+    ctx.beginPath();
+    ctx.strokeStyle = d3.color('gray').darker(0.5);
+    data.graticuleLines.forEach(x => geoPath(x));
+    ctx.stroke();
+
+    // milky way
+    if (showMilkyWay) {
+      data.milkyWay.forEach(x => {
+        ctx.beginPath();
+        ctx.fillStyle ='rgba(255, 255, 255, .05)';
+        geoPath(x);
+        ctx.fill();
+      })
+    }
+
+    // constellation bounds
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = d3.color('lightskyblue').darker(1.5);
+    data.constellationBounds.forEach(x => {
+      ctx.beginPath();
+      ctx.lineWidth = 1.5;
+      geoPath(x.geometry);
+      ctx.stroke();
+    })
+
+    // constellation lines
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = d3.color('gray');
+    data.constellationLines.forEach(x => {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      geoPath(x.geometry);
+      ctx.stroke();
+    })
+
+    // asterism lines
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = d3.color('darkseagreen');
+    data.asterisms.forEach(x => {
+      ctx.beginPath();
+      geoPath(x.geometry);
+      ctx.stroke();
+    })
+
+    // messier objects
+    ctx.strokeStyle = d3.color('red');
+    data.messier.forEach(x => {
+      const coords = x.geometry.coordinates;
+      if (isPointVisible(projection, coords) && x.properties.mag < 6 * zoomFactor()) {
+        const pixelCoords = projection(coords);
+        ctx.beginPath();
+        ctx.arc(pixelCoords[0], pixelCoords[1], 4, 0, 2 * Math.PI);
+        ctx.stroke();
+          // log(coords)
+          ctx.font = '15px sans-serif';
+          ctx.fillStyle = d3.color('red').darker(.5);
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'bottom';
+          const dsoname = data.dsonames[x.properties.desig]
+        ctx.fillText(x.properties.name, pixelCoords[0], pixelCoords[1] - 6);
+        if (dsoname && dsoname.de) {
+          ctx.fillText(dsoname.de, pixelCoords[0], pixelCoords[1] + 20);
+        }
+      }
+    })
+
+    // stars
+    data.stars.forEach(x => {
+      const coords = x.geometry.coordinates;
+      if (isPointVisible(projection, coords)) {
+        ctx.beginPath();
+        ctx.fillStyle = x.properties.color;
+        const pcoords = projection(coords);
+        ctx.arc(pcoords[0], pcoords[1], x.properties.pointRadius, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    })
+
+    // star names
+    data.selectedStars.forEach(x => {
+      const starNames = data.starNames[x.id];
+      let starName;
+      if (starNames?.de) {
+        starName = starNames.de
+      } else if (starNames?.name && starNames.name.length > 3 && x.properties.pointRadius > 2 ) {
+        starName = starNames.name
+      } else if (starNames?.bayer && x.properties.pointRadius > 2) {
+        starName = starNames.bayer
+      } else if (starNames?.flam && x.properties.pointRadius > 2) {
+        starName = starNames.flam
+      } else if (starNames?.desig && x.properties.pointRadius > 2) {
+        starName = starNames.desig
+      }
+      if (starName) {
+        ctx.beginPath();
+        const coordinates = x.geometry.coordinates;
+        if (isPointVisible(projection, coordinates)) {
+          const pixelCoords = projection([coordinates[0], coordinates[1]]);
+          // log(coords)
+          ctx.font = '15px sans-serif';
+          ctx.fillStyle = d3.color('darkkhaki').darker(.5);
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'bottom';
+          ctx.fillText(starName, pixelCoords[0], pixelCoords[1] - x.properties.pointRadius);
+        }
+      }
+    })
+
+    // graticule labels
     data.graticuleLines.forEach(x => {
         if (x.coordinates[0][0] === x.coordinates[1][0]) {
           // lon
           const coords = x.coordinates[Math.floor(x.coordinates.length / 2)];
-          if (coords[0] %90 !== 0) {
+          if (coords[0] % 90 !== 0) {
             for (let i = -60; i <= 60; i += 30) {
               renderTextGraticule(
                 projection,
@@ -80,51 +212,33 @@ export default async () => {
             }
           })
         }
-        geoPath(x);
       }
     );
-    ctx.stroke();
-
-    // constellation lines
-    data.constellationLines.forEach(x => {
-      ctx.beginPath();
-      geoPath(x.geometry);
-      ctx.stroke();
-    })
-
-    // stars
-    data.selectedStars.forEach(x => {
-      ctx.fillStyle = x.properties.color;
-      ctx.beginPath();
-      geoPath(x.geometry, x.properties.pointRadius);
-      // context.arc(pt[0], pt[1], s.r * projection.scale() * .002, 0, 2 * Math.PI);
-      ctx.fill();
-      const starName = data.starNames[x.id]?.name;
-      if (starName && x.properties.pointRadius > 2.4) {
-        const coordinates = x.geometry.coordinates;
-        if (isPointVisible(projection, coordinates)) {
-          const pixelCoords = projection([coordinates[0], coordinates[1]]);
-          // log(coords)
-          ctx.font = '14px sans-serif';
-          ctx.fillStyle = d3.color('darkkhaki');
-          ctx.textAlign = 'right';
-          ctx.textBaseline = 'bottom';
-          ctx.fillText(starName, pixelCoords[0], pixelCoords[1]);
-        }
-      }
-    })
 
     // constellation names
     data.constellations.forEach(x => {
       const coordinates = x.geometry.coordinates;
       if (isPointVisible(projection, coordinates)) {
         const pixelCoords = projection([coordinates[0], coordinates[1]]);
-        ctx.font = '14px sans-serif';
-        ctx.fillStyle = d3.color('darkcyan');
+        ctx.font = '16px sans-serif';
+        ctx.fillStyle = d3.color('lightskyblue').darker(1.5);
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
         ctx.fillText(x.properties.de, pixelCoords[0], pixelCoords[1]);
       }
+    })
+
+    // asterism names
+    data.asterisms.forEach(x => {
+      const pixelCoords = geoPath.centroid(x);
+      // if (isPointVisible(projection, coordinates)) {
+      //   const pixelCoords = projection([coordinates[0], coordinates[1]]);
+      ctx.font = '14px sans-serif';
+      ctx.fillStyle = d3.color('darkseagreen');
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(x.properties.de, pixelCoords[0], pixelCoords[1]);
+      // }
     })
   }
   const onScaleChanged = scaleFactor => {
@@ -141,23 +255,10 @@ export default async () => {
     projection
       .translate([width / 2, height / 2])
       .scale(Math.min(width, height) / 2);
-    zoomer.unityScale = projection.scale();
+    unityScale = projection.scale();
+    zoomer.unityScale = unityScale;
     render();
   }
-
-
-  function updateRotationBasedOnTime() {
-    const now = new Date();
-    const hour = now.getUTCHours() + now.getUTCMinutes() / 60;
-
-    // Berechne Sternzeit (vereinfacht)
-    const siderealTime = (hour * 15) % 360; // 15° pro Stunde
-
-    // Setze Rotation für aktuellen Himmelsausschnitt
-    projection.rotate([siderealTime, viewerLatitude, 0]);
-    render();
-  }
-
 
   onScaleChanged(1);
   onResize();
